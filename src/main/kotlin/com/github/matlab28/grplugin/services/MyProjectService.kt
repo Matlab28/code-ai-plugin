01f599fc61b0a28d417631958deca732d5e0919c
GitHub Action <action@github.com>
2026-08-07 19:57:55 +0000

    Template cleanup

--- src/main/kotlin/com/github/matlab28/grplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/matlab28/grplugin/services/MyProjectService.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.services
+package com.github.matlab28.grplugin.services
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.matlab28.grplugin.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
